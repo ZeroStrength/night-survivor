fafe42158fb42d946eb844b694a9b6af8728b4de
ZeroStrength <brownbearpower@gmail.com>
2021-01-17 18:20:15 +0900

    feat: initial datetime state in todo component

--- pages/Todo.js
+++ pages/Todo.js
@@ -1,20 +1,11 @@
-import * as React from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import EditorScreen from './components/Editor';
+import * as React from "react"
+import { createStackNavigator } from "@react-navigation/stack"
+import EditorScreen from "./components/Editor"
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 function TodoScreen() {
-  return (
-    <>
-      <View>
-        <TextInput placeholder="Title:" />
-      </View>
-      <EditorScreen />
-      <Button title="Save" />
-    </>
-  );
+  return <EditorScreen />
 }
 
 export default function Todo() {
@@ -23,8 +14,8 @@ export default function Todo() {
       <Stack.Screen
         name="ToDo"
         component={TodoScreen}
-        options={{ title: 'Night Survivor > ToDo' }}
+        options={{ title: "Night Survivor > ToDo" }}
       />
     </Stack.Navigator>
-  );
+  )
 }
--- pages/components/Editor.js
+++ pages/components/Editor.js
@@ -1,11 +1,67 @@
-import React, { useRef, useState } from "react";
-import { StyleSheet, ScrollView } from "react-native";
-import { defaultActions, RichEditor, RichToolbar } from "react-native-pell-rich-editor";
+import React, { useRef, useState } from "react"
+import {
+  Text,
+  Button,
+  View,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import DateTimePicker from "@react-native-community/datetimepicker"
+import {
+  defaultActions,
+  RichEditor,
+  RichToolbar
+} from "react-native-pell-rich-editor"
+import Toast from "react-native-toast-message"
 
 const EditorScreen = ({}) => {
   // const strikethrough = require("./assets/strikethrough.png"); //icon for strikethrough
-  const RichText = useRef(); //reference to the RichEditor component
-  const [article, setArticle] = useState("");
+  const RichText = useRef() //reference to the RichEditor component
+  const [article, setArticle] = useState("")
+  const [text, onChangeText] = useState("")
+  const [date, setDate] = useState(new Date())
+  const [startDate, setStartDate] = useState(new Date())
+  const [startTime, setStartTime] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+  const [endTime, setEndTime] = useState(new Date())
+  const [show, setShow] = useState(false)
+  const [mode, setMode] = useState("date")
+  const [isStart, setIsStart] = useState(true)
+
+  const onChange = (event, input) => {
+    setShow(Platform.OS === "ios")
+    if (mode === "date" && isStart) {
+      setStartDate(input)
+    } else if (mode === "date") {
+      setEndDate(input)
+    } else if (mode === "time" && isStart) {
+      setStartTime(input)
+    } else if (mode === "time") {
+      setEndTime(input)
+    }
+  }
+
+  const showMode = (currentMode) => {
+    setShow(true)
+    setMode(currentMode)
+  }
+
+  const showDatepicker = (isStart) => {
+    if (isStart) setDate(startDate)
+    else setDate(endDate)
+
+    setIsStart(isStart)
+    showMode("date")
+  }
+
+  const showTimepicker = (isStart) => {
+    if (isStart) setDate(startDate)
+    else setDate(endDate)
+
+    setIsStart(isStart)
+    showMode("time")
+  }
 
   // this function will be called when the editor has been initialized
   function editorInitializedCallback() {
@@ -15,7 +71,7 @@ const EditorScreen = ({}) => {
       //   "Toolbar click, selected items (insert end callback):",
       //   items
       // );
-    });
+    })
   }
 
   // Callback after height change
@@ -25,77 +81,137 @@ const EditorScreen = ({}) => {
 
   function onPressAddImage() {
     // you can easily add images from your gallery
-    RichText.current?.insertImage(
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/100px-React-icon.svg.png"
-    );
+    // RichText.current?.insertImage(
+    //   "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/100px-React-icon.svg.png"
+    // )
+    Toast.show({
+      position: "bottom",
+      text1: "Not available now..",
+      bottomOffset: 65
+    })
   }
 
   return (
-      <>
-        <ScrollView style={styles.container}>
-        <RichEditor
-            disabled={false}
-            containerStyle={styles.editor}
-            ref={RichText}
-            style={styles.rich}
-            placeholder={"Task todo here"}
-            onChange={(text) => setArticle(text)}
-            editorInitializedCallback={editorInitializedCallback}
-            onHeightChange={handleHeightChange}
+    <>
+      <View>
+        <TextInput
+          style={styles.titleInput}
+          value={text}
+          onChangeText={(text) => onChangeText(text)}
+          placeholder="Title:"
         />
-        </ScrollView>
+      </View>
+      <View style={styles.datetimeLayout}>
+        <View style={{ flex: 0.2 }}>
+          <Text>Start</Text>
+        </View>
+        <View style={{ flex: 0.4 }}>
+          <Text onPress={() => showDatepicker(true)}>
+            {startDate.toISOString().split("T")[0]}
+          </Text>
+        </View>
+        <View style={{ flex: 0.4 }}>
+          <Text onPress={() => showTimepicker(true)}>
+            {startTime.toTimeString().split(" ")[0]}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.datetimeLayout}>
+        <View style={{ flex: 0.2 }}>
+          <Text>End</Text>
+        </View>
+        <View style={{ flex: 0.4 }}>
+          <Text onPress={() => showDatepicker(false)}>
+            {endDate.toISOString().split("T")[0]}
+          </Text>
+        </View>
+        <View style={{ flex: 0.4 }}>
+          <Text onPress={() => showTimepicker(false)}>
+            {endTime.toTimeString().split(" ")[0]}
+          </Text>
+        </View>
+      </View>
+      {show && (
+        <View>
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={true}
+            display="spinner"
+            onChange={onChange}
+          />
+        </View>
+      )}
+      <ScrollView style={styles.container}>
         <RichToolbar
-            style={[styles.richBar]}
-            editor={RichText}
-            disabled={false}
-            iconTint={"black"}
-            selectedIconTint={"purple"}
-            disabledIconTint={"black"}
-            onPressAddImage={onPressAddImage}
-            iconSize={25}
-            actions={[
-            ...defaultActions,
-            ]}
+          style={[styles.richBar]}
+          editor={RichText}
+          disabled={false}
+          iconTint={"black"}
+          selectedIconTint={"purple"}
+          disabledIconTint={"black"}
+          onPressAddImage={onPressAddImage}
+          iconSize={25}
+          actions={[...defaultActions]}
         />
+        <RichEditor
+          disabled={false}
+          containerStyle={styles.editor}
+          ref={RichText}
+          style={styles.rich}
+          placeholder={"Task todo here"}
+          onChange={(text) => setArticle(text)}
+          editorInitializedCallback={editorInitializedCallback}
+          onHeightChange={handleHeightChange}
+        />
+      </ScrollView>
     </>
-  );
-};
+  )
+}
 
-export default EditorScreen;
+export default EditorScreen
 
 const styles = StyleSheet.create({
   a: {
     fontWeight: "bold",
-    color: "navy",
+    color: "navy"
   },
   div: {
-    fontFamily: "monospace",
+    fontFamily: "monospace"
   },
   p: {
-    fontSize: 30,
+    fontSize: 30
   },
   /*******************************/
   container: {
     flex: 1,
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent"
+  },
+  titleInput: {
+    height: 40,
+    borderColor: "#aaa",
+    backgroundColor: "#fff",
+    padding: 10
   },
   editor: {
-    backgroundColor: "black",
+    backgroundColor: "black"
   },
   rich: {
-    minHeight: 450,
-    flex: 1,
+    minHeight: 350,
+    flex: 1
   },
   richBar: {
     height: 35,
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent"
   },
   text: {
     fontWeight: "bold",
-    fontSize: 20,
+    fontSize: 20
   },
   tib: {
     textAlign: "center",
-    color: "#515156",
+    color: "#515156"
   },
-});
+  datetimeLayout: { flexDirection: "row", margin: 20 }
+})
