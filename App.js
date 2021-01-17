c17c6f95bcf7f0b0e0b4f9788291baf0feec96d8
ZeroStrength <brownbearpower@gmail.com>
2021-01-17 16:23:20 +0900

    chore: prettier, gitignore, app package name

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import Calendar from './pages/Calendar';
-import Todo from './pages/Todo';
-import Settings from './pages/Settings';
+import * as React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Ionicons from "react-native-vector-icons/Ionicons"
+import Calendar from "./pages/Calendar"
+import Todo from "./pages/Todo"
+import Settings from "./pages/Settings"
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
@@ -14,22 +14,20 @@ export default function App() {
       <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (route.name === 'ToDo') {
-              iconName = focused
-                ? 'checkbox'
-                : 'checkbox-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'ios-settings' : 'ios-settings-outline';
-            } else if (route.name === 'Calendar') {
-              iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
+            let iconName
+            if (route.name === "ToDo") {
+              iconName = focused ? "checkbox" : "checkbox-outline"
+            } else if (route.name === "Settings") {
+              iconName = focused ? "ios-settings" : "ios-settings-outline"
+            } else if (route.name === "Calendar") {
+              iconName = focused ? "ios-calendar" : "ios-calendar-outline"
             }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
+            return <Ionicons name={iconName} size={size} color={color} />
+          }
         })}
         tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray"
         }}
       >
         <Tab.Screen name="ToDo" component={Todo} />
@@ -38,5 +36,5 @@ export default function App() {
         <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
-  );
+  )
 }
