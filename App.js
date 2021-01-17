6122bd9fefdc7908f8d987e755733eb1c0da066a
ZeroStrength <brownbearpower@gmail.com>
2021-01-17 17:46:23 +0900

    feat(package): add toast popup package

--- App.js
+++ App.js
@@ -5,36 +5,40 @@ import Ionicons from "react-native-vector-icons/Ionicons"
 import Calendar from "./pages/Calendar"
 import Todo from "./pages/Todo"
 import Settings from "./pages/Settings"
+import Toast from "react-native-toast-message"
 
 const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName
-            if (route.name === "ToDo") {
-              iconName = focused ? "checkbox" : "checkbox-outline"
-            } else if (route.name === "Settings") {
-              iconName = focused ? "ios-settings" : "ios-settings-outline"
-            } else if (route.name === "Calendar") {
-              iconName = focused ? "ios-calendar" : "ios-calendar-outline"
+    <>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName
+              if (route.name === "ToDo") {
+                iconName = focused ? "checkbox" : "checkbox-outline"
+              } else if (route.name === "Settings") {
+                iconName = focused ? "ios-settings" : "ios-settings-outline"
+              } else if (route.name === "Calendar") {
+                iconName = focused ? "ios-calendar" : "ios-calendar-outline"
+              }
+              return <Ionicons name={iconName} size={size} color={color} />
             }
-            return <Ionicons name={iconName} size={size} color={color} />
-          }
-        })}
-        tabBarOptions={{
-          activeTintColor: "tomato",
-          inactiveTintColor: "gray"
-        }}
-      >
-        <Tab.Screen name="ToDo" component={Todo} />
-        {/* <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarBadge: 3 }} /> */}
-        <Tab.Screen name="Calendar" component={Calendar} />
-        <Tab.Screen name="Settings" component={Settings} />
-      </Tab.Navigator>
-    </NavigationContainer>
+          })}
+          tabBarOptions={{
+            activeTintColor: "tomato",
+            inactiveTintColor: "gray"
+          }}
+        >
+          <Tab.Screen name="ToDo" component={Todo} />
+          {/* <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarBadge: 3 }} /> */}
+          <Tab.Screen name="Calendar" component={Calendar} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </NavigationContainer>
+      <Toast ref={(ref) => Toast.setRef(ref)} />
+    </>
   )
 }
