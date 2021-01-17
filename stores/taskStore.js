cad34443d466974f4fd53a6787c525982ae33214
ZeroStrength <brownbearpower@gmail.com>
2021-01-17 16:37:46 +0900

    fix(store): add basic tasks store

---
+++ stores/taskStore.js
@@ -0,0 +1,28 @@
+import { observable } from "mobx"
+import { create, persist } from "mobx-persist"
+import AsyncStorage from "@react-native-community/async-storage"
+
+const store = observable({
+  tasks: [],
+
+  addTask(task) {
+    this.tasks.push(task)
+  }
+})
+
+const schema = {
+  tasks: {
+    type: "list"
+  }
+}
+
+const hydrate = create({
+  storage: AsyncStorage,
+  jsonify: true
+})
+
+const taskStore = persist(schema)(store)
+
+hydrate("tasks", taskStore)
+
+export default taskStore
