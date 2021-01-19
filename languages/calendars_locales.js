7223fc122d776043bbf4f727893807a95fe41dcd
ZeroStrength <brownbearpower@gmail.com>
2021-01-19 23:52:04 +0900

    feat: set calendar locale

---
+++ languages/calendars_locales.js
@@ -0,0 +1,85 @@
+import { LocaleConfig } from "react-native-calendars"
+
+LocaleConfig.locales["ko"] = {
+  monthNames: [
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+    "5월",
+    "6월",
+    "7월",
+    "8월",
+    "9월",
+    "10월",
+    "11월",
+    "12월"
+  ],
+  monthNamesShort: [
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+    "5월",
+    "6월",
+    "7월",
+    "8월",
+    "9월",
+    "10월",
+    "11월",
+    "12월"
+  ],
+  dayNames: [
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일"
+  ],
+  dayNamesShort: ["일", "월", "화", "수", "목", "금", "토"],
+  today: "오늘"
+}
+
+LocaleConfig.locales["en"] = {
+  monthNames: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ],
+  monthNamesShort: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ],
+  dayNames: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ],
+  dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+  today: "Today"
+}
--- pages/Calendar.js
+++ pages/Calendar.js
@@ -1,46 +1,191 @@
+import _ from "lodash"
 import * as React from "react"
 import { observer } from "mobx-react"
-import { Button, Text, View } from "react-native"
-import { CalendarList } from "react-native-calendars"
+import {
+  Platform,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+  Button,
+  Text,
+  View
+} from "react-native"
+import {
+  ExpandableCalendar,
+  AgendaList,
+  CalendarProvider,
+  LocaleConfig
+} from "react-native-calendars"
 import { createStackNavigator } from "@react-navigation/stack"
 import taskStore from "../stores/taskStore"
+import settingsStore from "../stores/settingsStore"
+import "../languages/calendars_locales"
+
+const testIDs = require("../testIDs")
+
+const themeColor = "#00AAAF"
+const lightThemeColor = "#EBF9F9"
+
+const ITEMS = [
+  {
+    title: "2021-01-10",
+    data: [{ hour: "8", duration: "abc", title: "First Yoga" }]
+  },
+  {
+    title: "2021-01-15",
+    data: [
+      { hour: "4pm", duration: "1h", title: "Pilates ABC" },
+      { hour: "5pm", duration: "1h", title: "Vinyasa Yoga" }
+    ]
+  },
+  {
+    title: "2021-01-20",
+    data: [
+      { hour: "1pm", duration: "1h", title: "Ashtanga Yoga" },
+      { hour: "2pm", duration: "1h", title: "Deep Streches" },
+      { hour: "3pm", duration: "1h", title: "Private Yoga" }
+    ]
+  }
+]
 
 const Stack = createStackNavigator()
 
 function HorizontalCalendarList() {
-  const [selectedDate, setSelectedDate] = React.useState("2021-01-16")
-  const [markedDates, setMarkedDates] = React.useState({})
+  // const [items, setItems] = React.useState([])
+  LocaleConfig.defaultLocale = settingsStore.getLanguage()
 
-  const setNewDaySelected = (date) => {
-    const markedDate = Object.assign({})
-    markedDate[date] = {
-      selected: true,
-      selectedColor: "#DFA460"
+  const onDateChanged = (date, updateSource) => {
+    console.log("ExpandableCalendarScreen onDateChanged: ", date, updateSource)
+  }
+
+  const onMonthChange = (month, updateSource) => {
+    console.log("ExpandableCalendarScreen onMonthChange: ", month, updateSource)
+  }
+
+  const buttonPressed = () => {}
+
+  const itemPressed = (id) => {
+    Alert.alert(id)
+  }
+
+  const renderEmptyItem = () => {
+    return (
+      <View style={styles.emptyItem}>
+        <Text style={styles.emptyItemText}>일정이 없습니다</Text>
+      </View>
+    )
+  }
+
+  const renderItem = ({ item }) => {
+    if (_.isEmpty(item)) {
+      return renderEmptyItem()
     }
-    setSelectedDate(date)
-    setMarkedDates(markedDate)
+
+    return (
+      <TouchableOpacity
+        onPress={() => itemPressed(item.title)}
+        style={styles.item}
+        testID={testIDs.agenda.ITEM}
+      >
+        <View>
+          <Text style={styles.itemHourText}>{item.hour}</Text>
+          <Text style={styles.itemDurationText}>{item.duration}</Text>
+        </View>
+        <Text style={styles.itemTitleText}>{item.title}</Text>
+        <View style={styles.itemButtonContainer}>
+          <Button color={"grey"} title={"보기"} onPress={buttonPressed} />
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  const getMarkedDates = () => {
+    const marked = {}
+    ITEMS.forEach((item) => {
+      // NOTE: only mark dates with data
+      if (item.data && item.data.length > 0 && !_.isEmpty(item.data[0])) {
+        marked[item.title] = { marked: true }
+      } else {
+        marked[item.title] = { disabled: true }
+      }
+    })
+    return marked
+  }
+
+  const getTheme = () => {
+    const disabledColor = "grey"
+
+    return {
+      // arrows
+      arrowColor: "black",
+      arrowStyle: { padding: 0 },
+      // month
+      monthTextColor: "black",
+      textMonthFontSize: 16,
+      textMonthFontFamily: Platform.OS === "android" ? "Roboto" : "Helvetica",
+      textMonthFontWeight: "bold",
+      // day names
+      textSectionTitleColor: "black",
+      textDayHeaderFontSize: 12,
+      textDayHeaderFontFamily:
+        Platform.OS === "android" ? "Roboto" : "Helvetica",
+      textDayHeaderFontWeight: "normal",
+      // dates
+      dayTextColor: themeColor,
+      textDayFontSize: 18,
+      textDayFontFamily: Platform.OS === "android" ? "Roboto" : "Helvetica",
+      textDayFontWeight: "500",
+      textDayStyle: { marginTop: Platform.OS === "android" ? 2 : 4 },
+      // selected date
+      selectedDayBackgroundColor: themeColor,
+      selectedDayTextColor: "white",
+      // disabled date
+      textDisabledColor: disabledColor,
+      // dot (marked date)
+      dotColor: themeColor,
+      selectedDotColor: "white",
+      disabledDotColor: disabledColor,
+      dotStyle: { marginTop: -2 }
+    }
   }
 
   return (
-    <>
-      <CalendarList
-        markedDates={markedDates}
-        current={selectedDate}
-        pastScrollRange={24}
-        futureScrollRange={24}
-        horizontal
-        pagingEnabled
-        onDayPress={(day) => {
-          setNewDaySelected(day.dateString)
-        }}
+    <CalendarProvider
+      date={ITEMS[0].title}
+      onDateChanged={onDateChanged}
+      onMonthChange={onMonthChange}
+      showTodayButton
+      disabledOpacity={0.6}
+      theme={{
+        todayButtonTextColor: themeColor
+      }}
+      todayBottomMargin={16}
+    >
+      <ExpandableCalendar
+        monthFormat={"yyyy.MM"}
+        testID={testIDs.expandableCalendar.CONTAINER}
+        // horizontal={false}
+        // hideArrows
+        disablePan
+        // hideKnob
+        initialPosition={ExpandableCalendar.positions.OPEN}
+        calendarStyle={styles.calendar}
+        headerStyle={styles.calendar} // for horizontal only
+        // disableWeekScroll
+        theme={getTheme()}
+        disableAllTouchEventsForDisabledDays
+        firstDay={0}
+        markedDates={getMarkedDates()} // {'2019-06-01': {marked: true}, '2019-06-02': {marked: true}, '2019-06-03': {marked: true}};
+        leftArrowImageSource={require("../assets/images/previous.png")}
+        rightArrowImageSource={require("../assets/images/next.png")}
       />
-      <Button
-        title="Add task test"
-        onPress={() => taskStore.addTask({ aa: "good" })}
+      <AgendaList
+        sections={ITEMS}
+        extraData={{}}
+        renderItem={renderItem}
+        sectionStyle={styles.section}
       />
-      <Text>{JSON.stringify(taskStore.tasks)}</Text>
-      <Button title="Reset task test" onPress={() => taskStore.removeAll()} />
-    </>
+    </CalendarProvider>
   )
 }
 
@@ -57,3 +202,52 @@ export default function Calendar() {
     </Stack.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  calendar: {
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  section: {
+    backgroundColor: lightThemeColor,
+    color: "grey",
+    textTransform: "capitalize"
+  },
+  item: {
+    padding: 20,
+    backgroundColor: "white",
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgrey",
+    flexDirection: "row"
+  },
+  itemHourText: {
+    color: "black"
+  },
+  itemDurationText: {
+    color: "grey",
+    fontSize: 12,
+    marginTop: 4,
+    marginLeft: 4
+  },
+  itemTitleText: {
+    color: "black",
+    marginLeft: 16,
+    fontWeight: "bold",
+    fontSize: 16
+  },
+  itemButtonContainer: {
+    flex: 1,
+    alignItems: "flex-end"
+  },
+  emptyItem: {
+    paddingLeft: 20,
+    height: 52,
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgrey"
+  },
+  emptyItemText: {
+    color: "lightgrey",
+    fontSize: 14
+  }
+})
