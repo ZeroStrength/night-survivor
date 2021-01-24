d5940268a62bbe294598608eb3f74bc8ba6a5ade
ZeroStrength <brownbearpower@gmail.com>
2021-01-24 23:02:34 +0900

    feat: add task floating action button

--- pages/Todo.js
+++ pages/Todo.js
@@ -1,11 +1,35 @@
 import * as React from "react"
 import { createStackNavigator } from "@react-navigation/stack"
 import EditorScreen from "./components/Editor"
+import { FloatingAction } from "react-native-floating-action"
+import Favicon from "../assets/favicon.png"
 
 const Stack = createStackNavigator()
 
+const actions = [
+  {
+    text: "Add task",
+    name: "bt_addtask",
+    position: 0
+  }
+]
+
 function TodoScreen() {
-  return <EditorScreen />
+  const [openEditor, setOpenEditor] = React.useState(false)
+  return (
+    <>
+      {openEditor && <EditorScreen />}
+      {/* floating action button to add task */}
+      <FloatingAction
+        actions={actions}
+        onPressItem={(name) => {
+          if (name === "bt_addtask") {
+            setOpenEditor(true)
+          }
+        }}
+      />
+    </>
+  )
 }
 
 export default function Todo() {
