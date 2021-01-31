d4c6efc5b96a09e3076c555f00ff13de51ce1e39
ZeroStrength <brownbearpower@gmail.com>
2021-01-31 22:51:26 +0900

    feat: add task id inside add task function

--- stores/taskStore.js
+++ stores/taskStore.js
@@ -3,10 +3,11 @@ import { create, persist } from "mobx-persist"
 import AsyncStorage from "@react-native-community/async-storage"
 
 const store = observable({
+  id: 0,
   tasks: [],
 
   addTask(task) {
-    this.tasks.push(task)
+    this.tasks.push({ ...task, ...{ id: this.id++ } })
   },
 
   removeAll() {
